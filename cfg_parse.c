2421fdb9c18f3c2599b84ccb86e73f772872c1d5
Greg Kennedy <kennedy.greg@gmail.com>
2019-03-31 06:16:38 +0000

    Add a new cfg_prune() method, which drops any keys that do not match a provided list.
    git-svn-id: svn://svn.code.sf.net/p/cfg-parse/code/trunk@10 e1b99d47-c72f-43c0-a64e-e62f35d08303

--- cfg_parse.c
+++ cfg_parse.c
@@ -9,6 +9,10 @@
 /* for tolower, isspace */
 #include <ctype.h>
 
+#ifndef SIZE_MAX
+#define SIZE_MAX ((size_t)-1)
+#endif
+
 /* implementation details of (opaque) config structures */
 struct cfg_node
 {
@@ -75,11 +79,17 @@ static char* cfg_norm_key(const char* key)
   size_t i, len;
   char* tkey;
 
+  if (key == NULL) return NULL;
+
   /* trim input key */
   tkey = cfg_trim(key);
   /* Exclude empty key */
   len = strlen(tkey);
-  if (len == 0) { free(tkey); return NULL; }
+  if (len == 0)
+  {
+    free(tkey);
+    return NULL;
+  }
 
   /* Lowercase key */
   for (i = 0; i < len; i++)
@@ -89,11 +99,49 @@ static char* cfg_norm_key(const char* key)
 }
 
 /**
+ * This function initializes a cfg_struct, and must be called before
+ * performing any further operations.
+ * @return Pointer to newly initialized cfg_struct object.
+ */
+struct cfg_struct* cfg_init()
+{
+  struct cfg_struct* cfg;
+
+  cfg = (struct cfg_struct*)cfg_malloc(sizeof(struct cfg_struct));
+  cfg->head = NULL;
+
+  return cfg;
+}
+
+/**
+ * This function deletes an entire cfg_struct, clearing any memory
+ * previously held by the structure.
+ * @param cfg Pointer to cfg_struct to delete.
+ */
+void cfg_free(struct cfg_struct* cfg)
+{
+  struct cfg_node* temp;
+
+  if (cfg == NULL) return;
+
+  while ((temp = cfg->head) != NULL)
+  {
+    cfg->head = temp->next;
+
+    free(temp->key);
+    free(temp->value);
+    free(temp);
+  }
+
+  free(cfg);
+}
+
+/**
  * This function loads data from a file, and inserts / updates the specified cfg_struct.
  * New keys will be inserted.  Existing keys will have values overwritten by those read from the file.
  * The format of config-files is "key=value", with any amount of whitespace.
  * Comments can be included by using a # character: processing ends at that point.
- * The maximum line size is CFG_MAX_LINE-1 bytes (see cfg_parse.h)
+ * The maximum line size is CFG_MAX_LINE bytes (see cfg_parse.h)
  * @param cfg Pointer to cfg_struct to update.
  * @param filename String containing filename to open and parse.
  * @return EXIT_SUCCESS (0) on success, or EXIT_FAILURE if file could not be opened.
@@ -102,7 +150,7 @@ int cfg_load(struct cfg_struct* cfg, const char* filename)
 {
   FILE* fp;
   char* delim;
-  char buffer[CFG_MAX_LINE];
+  char buffer[CFG_MAX_LINE + 1];
 
   /* safety check: null input */
   if (cfg == NULL || filename == NULL) return EXIT_FAILURE;
@@ -113,7 +161,7 @@ int cfg_load(struct cfg_struct* cfg, const char* filename)
 
   while (!feof(fp))
   {
-    if (fgets(buffer, CFG_MAX_LINE, fp) != NULL)
+    if (fgets(buffer, CFG_MAX_LINE + 1, fp) != NULL)
     {
       /* locate first # sign and terminate string there (comment) */
       delim = strchr(buffer, '#');
@@ -289,6 +337,11 @@ void cfg_set(struct cfg_struct* cfg, const char* key, const char* value)
 void cfg_set_array(struct cfg_struct* cfg, const char* keys[], const char* values[], const size_t count)
 {
   size_t i;
+
+  /* safety check: null input */
+  if (cfg == NULL || keys == NULL || values == NULL) return;
+
+  /* Call cfg_set on every item in the lists */
   for (i = 0; i < count; i ++)
     cfg_set(cfg, keys[i], values[i]);
 }
@@ -354,46 +407,84 @@ void cfg_delete(struct cfg_struct* cfg, const char* key)
  * This function deletes multiple key-value pairs from a cfg_struct.
  * @param cfg Pointer to cfg_struct to search.
  * @param keys Array of strings containing key to search for.
- * @param count Length of keys / values arrays
+ * @param count Length of keys array
  */
 void cfg_delete_array(struct cfg_struct* cfg, const char* keys[], const size_t count)
 {
   size_t i;
+
+  /* safety check: null input */
+  if (cfg == NULL || keys == NULL) return;
+
+  /* Call cfg_delete on every item in the list */
   for (i = 0; i < count; i ++)
     cfg_delete(cfg, keys[i]);
 }
 
 /**
- * This function initializes a cfg_struct, and must be called before
- * performing any further operations.
- * @return Pointer to newly initialized cfg_struct object.
+ * This function performs the inverse of cfg_delete_array().
+ * Instead of deleting entries from cfg which match keys[],
+ *  this will KEEP only those entries that match keys[].
+ * It can be used to keep a config file tidy between versions or
+ *  after user edits.
+ * @param cfg Pointer to cfg_struct to search.
+ * @param keys Array of strings containing keys to keep
+ * @param count Length of keys array
  */
-struct cfg_struct* cfg_init()
+void cfg_prune(struct cfg_struct* cfg, const char* keys[], const size_t count)
 {
-  struct cfg_struct* cfg;
-  cfg = (struct cfg_struct*)cfg_malloc(sizeof(struct cfg_struct));
-  cfg->head = NULL;
-  return cfg;
-}
+  char** tkeys;
+  size_t i;
 
-/**
- * This function deletes an entire cfg_struct, clearing any memory
- * previously held by the structure.
- * @param cfg Pointer to cfg_struct to delete.
- */
-void cfg_free(struct cfg_struct* cfg)
-{
   struct cfg_node* cur;
-  struct cfg_node* temp;
+  struct cfg_node* prev;
 
+  /* safety check: null input */
+  if (cfg == NULL || keys == NULL || SIZE_MAX / sizeof(char*) < count) return;
+
+  /* First we must prep every key in keys[] using the normalize function. */
+  tkeys = (char**)cfg_malloc(count * sizeof(char*));
+  for (i = 0; i < count; i ++)
+    tkeys[i] = cfg_norm_key(keys[i]);
+
+  /* Now iterate through the cfg struct and test every entry */
+  /* set pointer to start of list */
   cur = cfg->head;
+
+  /* search list for existing key */
   while (cur != NULL)
   {
-    temp = cur->next;
-    free(cur->key);
-    free(cur->value);
-    free(cur);
-    cur = temp;
+    for (i = 0; i < count; i ++)
+      if (tkeys[i] != NULL && strcmp(tkeys[i], cur->key) == 0)
+        break;
+
+    if (i == count)
+    {
+      /* Didn't find a key match - delete this */
+      free(cur->value);
+      free(cur->key);
+
+      if (cur == cfg->head)
+      {
+        /* first element */
+        cfg->head = cur->next;
+        free(cur);
+        cur = cfg->head;
+      } else {
+        /* splice out element */
+        prev->next = cur->next;
+        free(cur);
+        cur = prev->next;
+      }
+    } else {
+      /* matched, advance list element */
+      prev = cur;
+      cur = cur->next;
+    }
   }
-  free (cfg);
+
+  /* Cleanup all our trimmed keys */
+  for (i = 0; i < count; i ++)
+    free(tkeys[i]);
+  free(tkeys);
 }
